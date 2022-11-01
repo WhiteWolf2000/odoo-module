ec537d361afc4a67b951a58cceca25d7c8c446f2
WhiteWolf2000 <99486516+WhiteWolf2000@users.noreply.github.com>
2022-11-01 11:37:53 +0500

    Update travel.py

--- custom_addons/travel_management/models/travel.py
+++ custom_addons/travel_management/models/travel.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from odoo import models, fields
 
 
@@ -8,14 +7,4 @@ class TravelProfile(models.Model):
     name = fields.Char(string="Traveler Name")
     email = fields.Char(string="Email")
     phone = fields.Char("Phone No")
-=======
-from odoo import models,fields,api
-
-class Travel(models.Model):
-    _name = 'travel.travel'
-
-    name = fields.Char(string='Name')
-    destination = fields.Char(char="Destination")
-    start_date = fields.Date(string="Start Date")
-    end_date = fields.Date(string="End Date")
->>>>>>> 4c74d74b71788ffcc3e3854ac43ee6ac8b05a57c
+    
